aadfdb1ee68720f9034ad37718ce3d3fd19a6173
David Masterson <davidmasterson@Davids-MacBook-Pro.local>
2021-02-06 18:31:18 -0600

    modified CSVWriter method to have multiple methods to create a csv object.

--- src/main/scala/com/project0/models/Rule.scala
+++
@@ -1,9 +0,0 @@
-package com.project0.models
-
-case class Rule(index: String, name: String, description: String, Subsections: List[Subsection])
-
-object Rule {
-  def getRuleInfo(): Unit ={
-    val ruleAPI = "https://www.dnd5eapi.co/api/rules"
-  }
-}
--- src/main/scala/com/project0/models/RuleCatagoryListItem.scala
+++
@@ -1,4 +0,0 @@
-package com.project0.models
-
-case class RuleCatagoryListItem(name: String, index: String)
-
